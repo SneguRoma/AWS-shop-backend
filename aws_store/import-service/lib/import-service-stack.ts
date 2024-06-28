362c5775384a4b567beb3cd1a74f039d72668d23
sneguroma <sneguroma@gmail.com>
2024-06-28 10:47:58 +0200

    feat: import-service-stack added

--- aws_store/import-service/lib/import-service-stack.ts
+++ aws_store/import-service/lib/import-service-stack.ts
@@ -1,16 +1,44 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import { Construct } from "constructs";
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketName = "my-bucket-to-import-service";
+    const existingBucket = s3.Bucket.fromBucketName(
+      this,
+      "ExistingBucket",
+      bucketName
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const importProductsFileLambda = new lambda.Function(
+      this,
+      "Get-Products-List-Lambda",
+      {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        code: lambda.Code.fromAsset("lambda"),
+        handler: "importProductsList.importProductsList",
+        environment: {
+          BUCKET_NAME: bucketName,
+        },
+      }
+    );
+
+    existingBucket.grantReadWrite(importProductsFileLambda);
+
+    const api = new apigw.RestApi(this, "ImportApi", {
+      restApiName: "Import Service",
+    });
+
+    const importResource = api.root.addResource("import");
+
+    importResource.addMethod(
+      "GET",
+      new apigw.LambdaIntegration(importProductsFileLambda)
+    );
   }
 }
