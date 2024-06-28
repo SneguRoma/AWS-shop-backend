65ef358587bc6ef9b753d75a3f52f5ee9a02e80a
sneguroma <sneguroma@gmail.com>
2024-06-28 14:05:15 +0200

    feat: importfileparser lambda  added

---
+++ aws_store/import-service/lambda/importFileParser.ts
@@ -0,0 +1,41 @@
+import { S3Handler } from "aws-lambda";
+import { S3Client, GetObjectCommand, CopyObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import * as csv from "csv-parser";
+import { Readable } from "stream";
+
+const s3Client = new S3Client({});
+
+export const handler: S3Handler = async (event) => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = event.Records[0].s3.object.key;
+
+  try {
+    const command = new GetObjectCommand({ Bucket: bucket, Key: key });
+    const response = await s3Client.send(command);
+    const stream = response.Body as Readable;
+
+    stream.pipe(csv())
+      .on("data", (data) => {
+        console.log("Parsed data:", data);
+      })
+      .on("end", () => {
+        console.log("File parsed successfully");
+      });
+
+      const newKey = key.replace('uploaded/', 'parsed/');
+        const copyCommand = new CopyObjectCommand({
+          Bucket: bucket,
+          CopySource: `${bucket}/${key}`,
+          Key: newKey,
+        });
+        await s3Client.send(copyCommand);
+        console.log(`File copied to ${newKey}`);
+
+        const deleteCommand = new DeleteObjectCommand({ Bucket: bucket, Key: key });
+        await s3Client.send(deleteCommand);
+        console.log(`File deleted from ${key}`);
+
+  } catch (error) {
+    console.error("Error parsing file:", error);
+  }
+};
--- aws_store/import-service/lib/import-service-stack.ts
+++ aws_store/import-service/lib/import-service-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as s3notifications from 'aws-cdk-lib/aws-s3-notifications';
 import { Construct } from "constructs";
 
 export class ImportServiceStack extends cdk.Stack {
@@ -33,12 +34,24 @@ export class ImportServiceStack extends cdk.Stack {
     const api = new apigw.RestApi(this, "ImportApi", {
       restApiName: "Import Service",
     });
-
     const importResource = api.root.addResource("import");
-
+    
+    const importFileParserLambda = new lambda.Function(this, 'importFileParserLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'importFileParser.importFileParser',
+      code: lambda.Code.fromAsset('lambda'),
+    });
+    
+    existingBucket.grantRead(importFileParserLambda);
+    existingBucket.addEventNotification(s3.EventType.OBJECT_CREATED_PUT, new s3notifications.LambdaDestination(importFileParserLambda), {
+      prefix: 'uploaded/',
+    });
+    
+    
     importResource.addMethod(
       "GET",
       new apigw.LambdaIntegration(importProductsFileLambda)
     );
+    
   }
 }
