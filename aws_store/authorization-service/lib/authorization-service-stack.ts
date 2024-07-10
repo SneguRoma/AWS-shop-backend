380915dbb72beda4424a8a2a788b53d6fbe8525b
sneguroma <sneguroma@gmail.com>
2024-07-10 15:50:30 +0200

    feat: autorization service and stack added

---
+++ aws_store/authorization-service/lib/authorization-service-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+export class AuthorizationServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const basicAuthorizer = new lambda.Function(this, 'BasicAuthorizer', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'basicAuthorizer.basicAuthorizer',
+      code: lambda.Code.fromAsset("lambda"),      
+    });
+  }
+}
+
+
+
+
